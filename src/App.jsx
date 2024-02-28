68733a633edacc3cc7208bcc93dcd5260b51d296
@jerscape <jeremydutton5@gmail.com>
2024-02-27 23:12:50 -0500

    created weather box and fetch for weatehr API, but getting error, so need to revise

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WeatherBox from './components/WeatherBox'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Weather Now</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,6 +25,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+      </div>
+      <div>
+        <WeatherBox/>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- src/components/WeatherBox.jsx
+++ src/components/WeatherBox.jsx
@@ -1,7 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+const API_KEY = import.meta.env.VITEREACT_APP_API_KEY
 
 const WeatherBox = () => {
-  return <div></div>
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    const fetchCurrentWeather = async (city) => {
+      try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+        if(!response.ok) {
+          throw new Error('Error fetching weather')
+        }
+
+        const data = await response.json();
+        return data
+      } catch (error) {
+        console.log("Error fetch weather data:", error)
+      }
+    }
+    fetchCurrentWeather('London')
+  }, [])
+
+
+
+  return <div><h1>I am the weather box!</h1></div>
 }
 
 export default WeatherBox
